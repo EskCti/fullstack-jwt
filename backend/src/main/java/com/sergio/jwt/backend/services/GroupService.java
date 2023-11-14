fb26660158d28770d1384b20b0410af1adcc7695
eskokado <eskokado@gmail.com>
2023-11-14 10:59:47 -0300

    feat: create GroupService

---
+++ backend/src/main/java/com/sergio/jwt/backend/services/GroupService.java
@@ -0,0 +1,25 @@
+package com.sergio.jwt.backend.services;
+
+import com.sergio.jwt.backend.entites.GroupEntity;
+import com.sergio.jwt.backend.repositories.GroupRepository;
+
+public class GroupService extends BaseService<GroupEntity, GroupRepository>{
+
+    @Override
+    public GroupEntity save(GroupEntity request) {
+        request = super.repository.save(request);
+        return request;
+    }
+
+    @Override
+    public void delete(Long id) {
+        repository.deleteById(id);
+        repository.flush();
+    }
+
+    @Override
+    public GroupEntity fetchOrFail(Long id) {
+        var result = repository.findById(id);
+        return result.orElse(null);
+    }
+}
