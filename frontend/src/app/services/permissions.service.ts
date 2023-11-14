6b13b0534ab33d728e0800ff0147604e804ddee2
eskokado <eskokado@gmail.com>
2023-11-14 15:35:05 -0300

    feat: create service permissions

---
+++ frontend/src/app/services/permissions.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {environment} from "../../environments/environment";
+import {HttpClient} from "@angular/common/http";
+import {Permission} from "../models/permission";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PermissionsService {
+  private _URL = environment.url  + "groups";
+
+  constructor(private http: HttpClient) { }
+
+  salvar(permission: Permission) {
+    return this.http.post<Permission>(this._URL, permission);
+  }
+
+  buscarTodos() {
+    return this.http.get<Permission[]>(this._URL);
+  }
+
+  remover(id: number) {
+    return this.http.delete(`${this._URL}/${id}`);
+  }
+}
