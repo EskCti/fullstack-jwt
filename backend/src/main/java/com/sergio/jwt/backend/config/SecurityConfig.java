719927bd06f9121181de805dca96224e46dfc457
eskokado <eskokado@gmail.com>
2023-11-14 16:34:36 -0300

    feat: add urls permition all into api/categories

--- backend/src/main/java/com/sergio/jwt/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/sergio/jwt/backend/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
                 .sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers(HttpMethod.POST, "/login", "/register").permitAll()
-                        .requestMatchers("/api/groups/**", "/api/permissions/**").permitAll()
+                        .requestMatchers("/api/groups/**", "/api/permissions/**", "/api/categories/**").permitAll()
                         .anyRequest().authenticated())
         ;
         return http.build();
