27061c83a09b87e3230803fe8a4be495d6fa884e
eskokado <eskokado@gmail.com>
2023-11-16 16:42:36 -0300

    feat: create model user

---
+++ frontend/src/app/models/user.ts
@@ -0,0 +1,8 @@
+export interface User {
+  id: number | undefined | null;
+  firstName: string | undefined | null;
+  lastName: string | undefined | null;
+  cpfCnpj: string | undefined | null;
+  career: string | undefined | null;
+  password: string | undefined | null;
+}
