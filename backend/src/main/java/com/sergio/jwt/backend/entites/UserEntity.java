56195f7d4d3e98bb0e0021d0f5bde0db2c12015a
eskokado <eskokado@gmail.com>
2023-11-16 16:34:44 -0300

    feat: add rel @ManyToMany user with group

--- backend/src/main/java/com/sergio/jwt/backend/entites/UserEntity.java
+++ backend/src/main/java/com/sergio/jwt/backend/entites/UserEntity.java
@@ -7,13 +7,16 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Data
 @Entity
 @Table(name = "app_user")
-public class UserEntity {
+public class UserEntity extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -37,4 +40,17 @@ public class UserEntity {
     @Column(nullable = false)
     @Size(max = 100)
     private String password;
+
+    @ManyToMany
+    @JoinTable(name = "app_user_group", joinColumns = @JoinColumn(name = "id_user"),
+            inverseJoinColumns = @JoinColumn(name = "id_group"))
+    private Set<GroupEntity> groups = new HashSet<>();
+
+    public boolean removeGroup(GroupEntity group) {
+        return getGroups().remove(group);
+    }
+
+    public boolean addGroup(GroupEntity group) {
+        return getGroups().add(group);
+    }
 }
