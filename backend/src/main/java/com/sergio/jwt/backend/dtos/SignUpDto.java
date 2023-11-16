9b3b2b34f05bd6f7c727157c7134086a71b6a85a
eskokado <eskokado@gmail.com>
2023-11-16 16:30:24 -0300

    fix: remove class SignUpDto

--- backend/src/main/java/com/sergio/jwt/backend/dtos/SignUpDto.java
+++
@@ -1,9 +0,0 @@
-package com.sergio.jwt.backend.dtos;
-
-public record SignUpDto (
-        String firstName,
-        String lastName,
-        String cpfCnpj,
-        String career,
-        char[] password
-) { }
